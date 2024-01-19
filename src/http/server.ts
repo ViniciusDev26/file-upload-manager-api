33a435432f657f54edb045b696bab71b08703f07
Carlos Vinicius <viniciusdev.26@gmail.com>
2024-01-19 06:45:46 -0300

    feat: configure project to deploy

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify"
 import cors from "@fastify/cors"
 import { makeFileController } from "./factories/makeFileController"
+import { env } from "../env"
 
 const app = fastify()
 app.register(cors)
@@ -10,6 +11,6 @@ app.get("/file/:id", fileController.getFileById.bind(fileController))
 app.post("/file/upload", fileController.upload.bind(fileController))
 
 app.listen({
-  port: 3000,
+  port: env.PORT ?? 3000,
   host: '0.0.0.0'
 })
