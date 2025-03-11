402f29c160ee2738711687f5a7dbbcc01b6020dd
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-03-10 22:42:41 -0300

    ✨ allow all files type

--- src/services/UploadFileService.ts
+++ src/services/UploadFileService.ts
@@ -7,53 +7,52 @@ interface UploadFileServiceParams {
   contentLength: number;
 }
 
-type UploadFileServiceResponse = {
-  type: "success"
-  id: string;
-  url: string;
-} | {
-  type: "error"
-  error: string;
-}
+type UploadFileServiceResponse =
+  | {
+    type: "success";
+    id: string;
+    url: string;
+  }
+  | {
+    type: "error";
+    error: string;
+  };
 
 export class UploadFileService {
-  async execute({name, contentType, contentLength}: UploadFileServiceParams): Promise<UploadFileServiceResponse> {
-    const key = `${Date.now()} - ${name}`
+  async execute({
+    name,
+    contentType,
+    contentLength,
+  }: UploadFileServiceParams): Promise<UploadFileServiceResponse> {
+    const key = `${Date.now()} - ${name}`;
     const maxLength = 1024 * 1024 * 1024; // 1GB
-    const allowedContentTypes = ["application/zip", "application/x-zip-compressed", "application/zip-compressed", "application/octet-stream", "application/x-rar-compressed", "application/rar", "application/x-rar", "application/x-tar", "application/x-7z-compressed", "application/x-gzip", "application/gzip", "application/x-bzip2", "application/x-bzip", "application/x-bz2", "application/x-bz", "application/x-tar", "application/x-compress"];
-    if (!allowedContentTypes.includes(contentType)) {
+
+    if (contentLength > maxLength) {
       return {
         type: "error",
-        error: "invalid content type"
-      }
+        error: "file too large",
+      };
     }
 
-    if(contentLength > maxLength) {
-      return {
-        type: "error",
-        error: "file too large"
-      }
-    }
-  
     const { url } = await generatePutPresignedUrl({
       key,
       contentType,
-      contentLength
-    })
-  
+      contentLength,
+    });
+
     const fileRow = await prisma.file.create({
       data: {
         name,
         key,
         contentType,
-        contentLength
-      }
-    })
+        contentLength,
+      },
+    });
 
     return {
       type: "success",
       id: fileRow.id,
       url,
-    }
+    };
   }
 }
