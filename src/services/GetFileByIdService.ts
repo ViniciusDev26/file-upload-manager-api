f23210c2ac4d9704331b06d91a72168dd3b649fc
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-03-11 00:00:49 -0300

    ♻️ remove `r2` not used import

--- src/services/GetFileByIdService.ts
+++ src/services/GetFileByIdService.ts
@@ -1,33 +1,35 @@
-import { prisma } from "../database/prisma"
-import { generateGetPresignedUrl, r2 } from "../lib/cloudflare-r2"
+import { prisma } from "../database/prisma";
+import { generateGetPresignedUrl } from "../lib/cloudflare-r2";
 
-type GetFileByIdServiceResponse = {
-  type: "success"
-  url: string
-} | {
-  type: "error"
-  error: string
-}
+type GetFileByIdServiceResponse =
+  | {
+    type: "success";
+    url: string;
+  }
+  | {
+    type: "error";
+    error: string;
+  };
 
 export class GetFileByIdService {
   async execute(id: string): Promise<GetFileByIdServiceResponse> {
     const file = await prisma.file.findUnique({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
     if (!file) {
       return {
         type: "error",
-        error: "file not found"
-      }
+        error: "file not found",
+      };
     }
-  
-    const { url } = await generateGetPresignedUrl(file.key)
 
-    return { 
+    const { url } = await generateGetPresignedUrl(file.key);
+
+    return {
       type: "success",
-      url
-    }
+      url,
+    };
   }
 }
